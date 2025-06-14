55dc21127da60fe0cc49b9ee1946f2f396aa2961
Fabricio Alvarenga <fab.oliveira.alvarenga@gmail.com>
2025-06-13 22:09:56 -0300

    Add navigation stack

--- Hidroly/Views/ContentView.swift
+++ Hidroly/Views/ContentView.swift
@@ -11,32 +11,34 @@ struct ContentView: View {
     @State private var activeTab: TabModel = .home
 
     var body: some View {
-        ZStack(alignment: .bottom) {
-            Group {
-                TabView(selection: $activeTab) {
-                    Tab.init(value: .home) {
-                        HomeView()
-                            .toolbarVisibility(.hidden, for: .tabBar)
-                    }
-                    
-                    Tab.init(value: .history) { 
-                        Text("Histórico")
-                            .toolbarVisibility(.hidden, for: .tabBar)
-                    }
+        NavigationStack {
+            ZStack(alignment: .bottom) {
+                Group {
+                    TabView(selection: $activeTab) {
+                        Tab.init(value: .home) {
+                            HomeView()
+                                .toolbarVisibility(.hidden, for: .tabBar)
+                        }
+                        
+                        Tab.init(value: .history) { 
+                            Text("Histórico")
+                                .toolbarVisibility(.hidden, for: .tabBar)
+                        }
 
-                    Tab.init(value: .notifications) {
-                        Text("Notificações")
-                            .toolbarVisibility(.hidden, for: .tabBar)
-                    }
+                        Tab.init(value: .notifications) {
+                            Text("Notificações")
+                                .toolbarVisibility(.hidden, for: .tabBar)
+                        }
 
-                    Tab.init(value: .settings) {
-                        Text("Configurações")
-                            .toolbarVisibility(.hidden, for: .tabBar)
+                        Tab.init(value: .settings) {
+                            Text("Configurações")
+                                .toolbarVisibility(.hidden, for: .tabBar)
+                        }
                     }
                 }
-            }
 
-            CustomTabBar(activeTab: $activeTab)
+                CustomTabBar(activeTab: $activeTab)
+            }
         }
     }
 }
