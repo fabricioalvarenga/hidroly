31967e605397aac66153507a5a5bdd77e3e128a1
Fabricio Alvarenga <fab.oliveira.alvarenga@gmail.com>
2025-06-17 22:08:31 -0300

    Add shadow and gradient

--- Hidroly/Components/ParameterButton.swift
+++ Hidroly/Components/ParameterButton.swift
@@ -37,9 +37,9 @@ struct ParameterButton: View {
             .padding(10)
             .background(alignment: .leading) {
                 Circle()
-                    .fill(manager.itemSelected ? Color.blue : Color.secondary)
+                    .fill(manager.itemSelected ? Color.blue.gradient : Color.secondary.gradient)
+                    .customShadow(opacity: 0.1, in: .circle)
             }
-            .shadow(radius: 2)
     }
     
     @ViewBuilder
@@ -59,42 +59,3 @@ struct ParameterButton: View {
     let manager = ParameterManager(parameterType: AgeType.self, title: "Idade", icon: Image(systemName: "person.crop.circle.badge.clock"))
     ParameterButton(manager: manager, textPosition: ParameterButton.TextPosition.bottom, action: {})
 }
-
-// import SwiftUI
-//
-// struct ParameterButton: View {
-//     let manager: any AnyParameterManager
-//     var position: CGPoint?
-//     let action: () -> Void
-//
-//    var body: some View {
-//         VStack {
-//             Button(action: action) {
-//                 manager.icon
-//                     .foregroundStyle(.white)
-//                     .padding(10)
-//                     .background(alignment: .leading) {
-//                         Circle()
-//                             .fill(manager.itemSelected ? Color.blue : Color.secondary)
-//                     }
-//                     .shadow(radius: 2)
-//             }
-//
-//             Text(manager.title)
-//                 .font(.caption2)
-//                 .foregroundStyle(manager.itemSelected ? Color.blue : Color.secondary)
-//                 .multilineTextAlignment(.center)
-//                 .lineLimit(2)
-//                 .frame(maxWidth: 80)
-//                 .minimumScaleFactor(0.8)
-//                 .fixedSize(horizontal: false, vertical: true)
-//            
-//         }
-//         .animation(.easeInOut(duration: 0.3), value: manager.selectedDisplayName)
-//    }
-// }
-//
-// #Preview {
-//     let manager = ParameterManager(parameterType: AgeType.self, title: "Idade", icon: Image(systemName: "person.crop.circle.badge.clock"))
-//     ParameterButton(manager: manager, action: {})
-// }
