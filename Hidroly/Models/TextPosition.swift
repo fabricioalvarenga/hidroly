08e38edc1952c213dc403a816afc3e22d3a30b88
Fabricio Alvarenga <fab.oliveira.alvarenga@gmail.com>
2025-06-27 22:46:52 -0300

    Circular button changes

--- Hidroly/Models/TextPosition.swift
+++ Hidroly/Models/TextPosition.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+enum TextPosition: String {
+    case top
+    case bottom
+    case trailing
+    case leading
+    
+    var rawValue: String {
+        switch self {
+        case .top: "top"
+        case .bottom: "bottom"
+        case .trailing: "trailing"
+        case .leading: "leading"
+        }
+    }
+}
+
--- Hidroly/Protocols/CircularButtonStyle.swift
+++ Hidroly/Protocols/CircularButtonStyle.swift
@@ -7,24 +7,22 @@
 
 import SwiftUI
 
-enum Position: String {
-    case top
-    case bottom
-    
-    var rawValue: String {
-        switch self {
-        case .top: "top"
-        case .bottom: "bottom"
-        }
-    }
-}
+protocol CircularButtonStyle {
+    associatedtype Content: View
+    associatedtype ShadowShape: InsettableShape
 
-protocol CircularButton: View {
-    associatedtype TextPosition: RawRepresentable where TextPosition.RawValue == String
-    associatedtype ButtonLabel: View
-    
-    var outsideText: String? { get }
+    var content: Content { get }
+    var diameter: CGFloat { get }
+    var ButtonForegroundStyle: AnyShapeStyle { get }
+    var ButtonBackgroundStyle: AnyShapeStyle { get }
+    var outsideText: String { get }
+    var outsideTextFont: Font? { get }
     var outsideTextMaxWidth: CGFloat? { get }
-    var size: CGFloat { get }
+    var outsideTextPosition: TextPosition? { get }
+    var outsideTextForegroundStyle: AnyShapeStyle { get }
+    var outsideTextAlignment: TextAlignment { get }
+    var outsideTextLineLimit: Int? { get }
+    var outsideTextMinimumScaleFactor: CGFloat { get }
+    var shadow: (color: Color, opacity: Double, radius: CGFloat, x: CGFloat, y: CGFloat, shape: ShadowShape)? { get }
     var action: () -> Void { get }
 }
