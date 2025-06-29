422ab1da0cd093daf4dde989fade81ef2c722701
Fabricio Alvarenga <fab.oliveira.alvarenga@gmail.com>
2025-06-29 16:17:30 -0300

    Add top circular text

--- Hidroly/Components/CircularTextView.swift
+++ Hidroly/Components/CircularTextView.swift
@@ -11,22 +11,35 @@ struct CircularTextView: View {
     let radius: CGFloat
     let text: String
     let angleStep: Double = 6.0
-
+    let textPosition: VerticalEdge
+    
     var body: some View {
         ZStack {
             ForEach(Array(text.enumerated()), id: \.offset) { index, char in 
-                let startAngle = Angle.degrees(-90 - (angleStep * Double(text.count - 1) / 2))
+                let startAngle = textPosition == .bottom ?
+                    Angle.degrees(-90 - (angleStep * Double(text.count - 1) / 2)) :
+                    Angle.degrees(90 - (angleStep * Double(text.count - 1) / 2))
+                
                 let offsetAngle = Angle.degrees(angleStep * Double(index))
 
-                let x = radius * cos(startAngle.radians + offsetAngle.radians)
+                let x = textPosition == .bottom ?
+                    radius * cos(startAngle.radians + offsetAngle.radians) :
+                    -radius * cos(startAngle.radians + offsetAngle.radians)
+                
                 let y = -radius * sin(startAngle.radians + offsetAngle.radians)
 
                 Text(String(char))
                     .font(.caption)
                     .foregroundStyle(Color.secondary)
-                    .rotationEffect(.degrees(-90) - startAngle - offsetAngle)
+                   .rotationEffect(textPosition == .bottom ?
+                                    (.degrees(-90) - startAngle - offsetAngle) :
+                                    (.degrees(270) + startAngle + offsetAngle))
                     .offset(x: x, y: y)
             }
         }
     }
+}
+
+#Preview {
+    CircularTextView(radius: 120, text: "INGERIDO ATÉ AGORA: 100 ml", textPosition: .bottom)
 }
